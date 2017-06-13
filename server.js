5a636ac636b5a8a7931bfbd090a6ec4f608af5f3
Lars Nolden <larsnolden@web.de>
2017-06-13 19:54:05 -0400

    added complete All and delete All actions

--- server.js
+++ server.js
@@ -2,18 +2,17 @@ const server = require('socket.io')();
 
 var Todo = require('./todo.js')
 
-let State = {
+var State = {
   todos: []
 }
 
 let id = 0;
 
 server.on('connection', function (client) {
-  console.log(State.todos)
+  console.log('client connected');
   server.emit('action', { type: 'LOAD_ALL', todos: State.todos })
 
-  console.log('client connected');
-  state = client.on('action', action => {
+  let reducer = (action) => {
     //remove 'server/' in front of action type -> actions are the same as on the client
     action.type = action.type.split('/')[1];
     console.log(`action: ${JSON.stringify(action)}`);
@@ -21,15 +20,25 @@ server.on('connection', function (client) {
       case 'ADD_TODO':
         let todo = new Todo(action.title, id++)
         server.emit('action', { type: 'ADD_TODO', todo });
-        return Object.assign({}, State, { todos: State.todos.push(todo) })
+        return Object.assign({}, State, { todos: State.todos.concat([todo]) })
       case 'REMOVE_TODO':
         server.emit('action', { type: 'REMOVE_TODO', id: action.id });
         return Object.assign({}, State, { todos: State.todos.filter(todo => todo.id !== action.id) })
       case 'SET_DONE_TODO':
         server.emit('action', { type: 'SET_DONE_TODO', id: action.id });
         return Object.assign({}, State, { todos: State.todos.map(todo => todo.id === action.id ? Object.defineProperty(todo, 'done', { value: (!todo.done) }) : todo) })
+      case 'DELETE_ALL':
+        server.emit('action', { type: 'DELETE_ALL' });
+        return { todos: [] }
+      case 'FINISH_ALL':
+        server.emit('action', { type: 'FINISH_ALL' });
+        return Object.assign({}, State, { todos: State.todos.map(todo => Object.defineProperty(todo, 'done', { value: (true) })) })
     }
-  })
+  }
+  client.on('action', (action) => {
+    State = reducer(action)
+    console.log(State)
+  });
 });
 
 console.log('Waiting for clients to connect');
